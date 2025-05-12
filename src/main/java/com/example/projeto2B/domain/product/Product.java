e1defd7c723e214fa12e543c4d0fbfd665bb7c0d
Felipe-Tomazoti <felipecesar005@gmail.com>
2025-05-12 15:08:25 -0300

    add: Product domain

---
+++ src/main/java/com/example/projeto2B/domain/product/Product.java
@@ -0,0 +1,60 @@
+package com.example.projeto2B.domain.product;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Table(name = "product")
+@Entity(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price;
+
+    public Product(ProductRequestDTO data){
+        this.price = data.price();
+        this.name = data.name();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
